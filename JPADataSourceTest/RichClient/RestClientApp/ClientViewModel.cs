fec725ede0240d15b2732f091c4e64857d55c609
K.M Hussain <km.hussain.in@gmail.com>
2021-01-09 11:57:21 +0530

    Code

---
+++ JPADataSourceTest/RichClient/RestClientApp/ClientViewModel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace RestClientApp
+{
+    public class ClientViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private OrderManagerClientModel model = new OrderManagerClientModel();
+
+        private string _customerId;
+        public string CustomerId 
+        { 
+            get => _customerId; 
+            set
+            {
+                _customerId = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CustomerId)));
+            }
+        }
+
+        private int _productNo;
+        public int ProductNo
+        {
+            get => _productNo;
+            set
+            {
+                _productNo = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ProductNo)));
+            }
+        }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                _quantity = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Quantity)));
+            }
+        }
+
+        private string _orderMessage = "Ready";
+        public string OrderMessage
+        {
+            get => _orderMessage;
+            set
+            {
+                _orderMessage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(OrderMessage)));
+            }
+        }
+
+        private IEnumerable<CustomerOrder> _customerOrders;
+        public IEnumerable<CustomerOrder> CustomerOrders
+        {
+            get => _customerOrders;
+            set
+            {
+                _customerOrders = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CustomerOrders)));
+            }
+        }
+
+        private bool CanExecuteOrder() => _customerId?.Length > 4;
+
+        private async void ExecuteOrder()
+        {
+            try
+            {
+                if (_quantity > 0)
+                {
+                    var info = await model.SubmitOrderAsync(_customerId, _productNo, _quantity);
+                    OrderMessage = $"New order number is {info.OrderNo}";
+                }
+                else
+                    OrderMessage = "";
+                CustomerOrders = await model.FetchOrdersAsync(_customerId, _productNo);
+            }
+            catch (ArgumentException ex)
+            {
+                OrderMessage = ex.Message;
+                CustomerOrders = null;
+            }
+
+        }
+
+        public ICommand Order => new OrderCommand { Parent = this };
+
+        class OrderCommand : ICommand
+        {
+            internal ClientViewModel Parent;
+
+            event EventHandler ICommand.CanExecuteChanged
+            {
+                add
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+
+                remove
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+
+            bool ICommand.CanExecute(object parameter)
+            {
+                return Parent.CanExecuteOrder();
+            }
+
+            void ICommand.Execute(object parameter)
+            {
+                Parent.ExecuteOrder();
+            }
+        }
+
+    }
+}
---
+++ JPADataSourceTest/RichClient/RestClientApp/OrderManagerClientModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace RestClientApp
+{
+   
+    public class CustomerOrder
+    {
+        public int? OrderNo { get; set; }
+
+        public string OrderDate { get; set; }
+
+        public int ProductNo { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+    public class OrderManagerClientModel
+    {
+        private HttpClient backend;
+
+        public OrderManagerClientModel()
+        {
+            backend = new HttpClient() { BaseAddress = new Uri(Properties.Settings.Default.OrderManagerServiceUri) };
+        }
+
+        public async Task<CustomerOrder> SubmitOrderAsync(string customerId, int productNo, int quantity)
+        {
+            var response = await backend.PostAsJsonAsync("orders", new
+            {
+                CustomerId = customerId,
+                ProductNo = productNo,
+                Quantity = quantity
+            });
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                return await response.Content.ReadFromJsonAsync<CustomerOrder>();
+            else
+            {
+                throw new ArgumentException(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        public async Task<IEnumerable<CustomerOrder>> FetchOrdersAsync(string customerId, int productNo)
+        {
+            try
+            {
+                var orders = await backend.GetFromJsonAsync<List<CustomerOrder>>($"orders/{customerId}");
+                orders.ForEach(e => e.OrderDate = e.OrderDate.Substring(0, 10));
+                return orders.FindAll(e => productNo > 0 ? e.ProductNo == productNo : true);
+            }
+            catch(HttpRequestException)
+            {
+                throw new ArgumentException("No orders found");
+            }
+        }
+
+    }
+}
